d5bb1f139d3e211f64eb72c45baad30509daefa0
‪Matan Levin‬‏ <103590081+Lenodeoss@users.noreply.github.com>
2024-07-25 14:26:40 +0300

    Add files via upload

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.bark();
+    }
+}
---
+++ demo/src/main/java/com/example/Dog.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Dog {
+    public void bark() {
+        System.out.println("Woof! Woof!");
+    }
+}
